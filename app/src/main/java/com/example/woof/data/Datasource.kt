4410e2f281c6e22a91b8dc64bb70d43119bacd03
Kristal Lopez <krystalv9809@gmail.com>
2025-05-19 12:09:28 -0600

    git commit -m "Add dummy data"

---
+++ app/src/main/java/com/example/woof/data/Datasource.kt
@@ -0,0 +1,21 @@
+package com.example.woof.data
+
+import com.example.woof.R
+import com.example.woof.model.Dog
+
+val dogs = listOf(
+    Dog(R.drawable.beagle, R.string.beagle, R.string.beagle_description),
+    Dog(R.drawable.belgian_shepherd, R.string.belgian_shepherd, R.string.belgian_shepherd_description),
+    Dog(R.drawable.boxer, R.string.boxer, R.string.boxer_description),
+    Dog(R.drawable.bulldog, R.string.bulldog, R.string.bulldog_description),
+    Dog(R.drawable.collie, R.string.collie, R.string.collie_description),
+    Dog(R.drawable.corgi, R.string.corgi, R.string.corgi_description),
+    Dog(R.drawable.doberman, R.string.doberman, R.string.doberman_description),
+    Dog(R.drawable.german_shepherd, R.string.german_shepherd, R.string.german_shepherd_description),
+    Dog(R.drawable.golden_retriever, R.string.golden_retriever, R.string.golden_retriever_description),
+    Dog(R.drawable.husky, R.string.husky, R.string.husky_description),
+    Dog(R.drawable.labrador, R.string.labrador, R.string.labrador_description),
+    Dog(R.drawable.poodle, R.string.poodle, R.string.poodle_description),
+    Dog(R.drawable.rottweiler, R.string.rottweiler, R.string.rottweiler_description),
+    Dog(R.drawable.shiba, R.string.shiba, R.string.shiba_description)
+)
---
+++ app/src/main/java/com/example/woof/model/Dog.kt
@@ -0,0 +1,10 @@
+package com.example.woof.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Dog(
+    @DrawableRes val imageId: Int,
+    @StringRes val breed: Int,
+    @StringRes val description: Int,
+    )
