c7a42c1b51ef9ad46097806e39ef21c3bc6d6c29
Kristal Lopez <krystalv9809@gmail.com>
2025-05-20 12:06:39 -0600

    git commit -m "Add dog species data and animations to show details"

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -4,25 +4,37 @@ package com.example.woof
 
 import android.content.res.Resources.Theme
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScaffoldDefaults
@@ -30,6 +42,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -44,6 +58,8 @@ import androidx.compose.ui.unit.dp
 import com.example.woof.data.dogs
 import com.example.woof.model.Dog
 import com.example.woof.ui.theme.WoofTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -107,16 +123,46 @@ fun WoofTopAppBar(modifier: Modifier = Modifier) {
 
 @Composable
 fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(modifier = modifier) {
-        Row (
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(id = R.dimen.padding_small)),
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            modifier = Modifier.animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness =  Spring.StiffnessMedium
+                )
+            )
         ) {
-            DogIcon(dog.imageId)
-            DogInformation(dog.breed, dog.description)
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(dimensionResource(id = R.dimen.padding_small)),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                DogIcon(dog.imageId)
+                DogInformation(dog.breed, dog.species)
+                Spacer(modifier = Modifier.weight((1f)))
+                DogItemButton(
+                    expanded = expanded,
+                    onClick = { expanded = !expanded }
+                )
+            }
+            if (expanded) {
+                DogDescription(
+                    dogDescription = dog.description,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(id = R.dimen.padding_medium),
+                        top = dimensionResource(id = R.dimen.padding_small),
+                        end = dimensionResource(id = R.dimen.padding_medium),
+                        bottom = dimensionResource(id = R.dimen.padding_small)
+                    )
+                )
+            }
         }
+
     }
 }
 
@@ -136,14 +182,42 @@ fun DogIcon(dogImage: Int, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun DogInformation(dogBreed: Int, dogDescription: Int, modifier: Modifier = Modifier) {
+fun DogInformation(dogBreed: Int, dogSpecies: Int, modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier
+        modifier = modifier.width(160.dp)
     ) {
         Text(
             text = stringResource(id = dogBreed),
             style = MaterialTheme.typography.displaySmall,
             fontWeight = FontWeight(700)
+        )
+        Text(
+            text = stringResource(id = dogSpecies),
+            style = MaterialTheme.typography.labelMedium
+        )
+    }
+}
+
+@Composable
+private fun DogItemButton(expanded: Boolean, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            contentDescription = stringResource(id = R.string.expand_button_content_description),
+            tint = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
+@Composable
+fun DogDescription(@StringRes dogDescription: Int, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = stringResource(id = R.string.description),
+            style = MaterialTheme.typography.labelSmall
         )
         Text(
             text = stringResource(id = dogDescription),
